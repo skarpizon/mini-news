d7f9f4edbb402f272c956a6edaacf861f0fa0173
Anton Skulkin <skarpizon@mail.ru>
2019-11-17 08:02:32 +0300

    Updates

---
+++ precache-manifest.f5923798a72c934da90757dc56387e3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd9c9462794fbd0429fef28b0d004f8",
+    "url": "/mini-news/index.html"
+  },
+  {
+    "revision": "918875f140e92f08a68e",
+    "url": "/mini-news/static/css/main.4d3df5b6.chunk.css"
+  },
+  {
+    "revision": "49963d2a07d5f96ec587",
+    "url": "/mini-news/static/js/2.2b031fe4.chunk.js"
+  },
+  {
+    "revision": "918875f140e92f08a68e",
+    "url": "/mini-news/static/js/main.ef15daef.chunk.js"
+  },
+  {
+    "revision": "404d1065513395323c2e",
+    "url": "/mini-news/static/js/runtime-main.26e09f4d.js"
+  }
+]);
