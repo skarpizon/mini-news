35593d3b5943b332589ecf9c683b3b110bbb0925
Anton Skulkin <skarpizon@mail.ru>
2019-11-17 08:36:11 +0300

    Updates

---
+++ precache-manifest.155a89b39b14a979e3a4ae23ed020130.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff03fe351740ced42bc61afba8f844b3",
+    "url": "/mini-news/index.html"
+  },
+  {
+    "revision": "de83873cb0f8c09976f8",
+    "url": "/mini-news/static/css/main.770e5243.chunk.css"
+  },
+  {
+    "revision": "ef44fb037cd2ec59d3ad",
+    "url": "/mini-news/static/js/2.7e66c125.chunk.js"
+  },
+  {
+    "revision": "de83873cb0f8c09976f8",
+    "url": "/mini-news/static/js/main.7e96bd49.chunk.js"
+  },
+  {
+    "revision": "404d1065513395323c2e",
+    "url": "/mini-news/static/js/runtime-main.26e09f4d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d242044a99daa181018493429d82674.js"
+  "/mini-news/precache-manifest.155a89b39b14a979e3a4ae23ed020130.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mini-news/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
